f934ae7e574dd898810efd28d1d16c7021ee2a51
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-06-09 00:48:19 +0200

    ResultsController IBOutlets, IBAction

--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var settingsLabel: UILabel!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+  @IBAction func recalculatePressed(_ sender: UIButton) {
+  }
+  
     /*
     // MARK: - Navigation
 
