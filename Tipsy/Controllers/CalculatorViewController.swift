d955d8165a24444febe417801e374e3c7d6edbf6
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-06-09 13:45:02 +0200

    % buttons changable, dropLast character 0%, trigger calculatePressed fce

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,20 +9,43 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-  @IBOutlet weak var billTextField: UITextField!
   
+  var tip = 0.1 // as default heighlighted 10%
+  
+  @IBOutlet weak var billTextField: UITextField!
   @IBOutlet weak var zeroPctButton: UIButton!
   @IBOutlet weak var tenPctButton: UIButton!
   @IBOutlet weak var twentyPctButton: UIButton!
   @IBOutlet weak var splitNumberLabel: UILabel!
   
   @IBAction func tipChanged(_ sender: UIButton) {
+    //  Deselect all tip buttons via IBOutlets
+    zeroPctButton.isSelected = false
+    tenPctButton.isSelected = false
+    twentyPctButton.isSelected = false
+    
+    //  Make the button that triggered the IBAction selected.
+    sender.isSelected = true
+    
+    // get info from pressed button
+    let buttonTitle = sender.currentTitle! // printed Optional("0%") /Strig?/ -> '!' -> String 0%
+    
+    // remove % character from String -> 0% -> 0; 10; 20
+    let buttonTitleNoCharacter = buttonTitle.dropLast()
+    
+    //  String into Double
+    let buttonIntoNumber = Double(buttonTitleNoCharacter)!
+    
+    // Convert into a decimal
+    tip = buttonIntoNumber / 100
   }
   
   @IBAction func stepperValueChanged(_ sender: UIStepper) {
   }
   
   @IBAction func calculatePressed(_ sender: UIButton) {
+    //  to get tip from tipChanged assign into declared variable above
+    print(tip)
   }
 }
 
